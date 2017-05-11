ec858ea901caf5d0074292595745d72c8de6e24a
wengzechao <1020028174@qq.com>
2017-05-11 23:23:14 +0800

    add EX09 and update index

--- js/EX09_1.js
+++ js/EX09_1.js
@@ -6,9 +6,19 @@ map.centerAndZoom(point, 15);
 map.addControl(new BMap.NavigationControl());   
 map.addControl(new BMap.ScaleControl());    
 map.addControl(new BMap.OverviewMapControl());    
-map.addControl(new BMap.MapTypeControl()); 
-map.enableScrollWheelZoom();   
+map.addControl(new BMap.MapTypeControl());    
+map.enableScrollWheelZoom();
 map.setCurrentCity("杭州"); 
 
 var marker = new BMap.Marker(point);// 创建标注    
 map.addOverlay(marker);
+
+var local = new BMap.LocalSearch(map, {
+	pageCapacity: 8,
+	renderOptions: {
+		 map: map,
+		 panel: "list"
+		  }
+		});
+local.search("宾馆");
+
--- js/EX09_2.js
+++ js/EX09_2.js
@@ -6,9 +6,17 @@ map.centerAndZoom(point, 15);
 map.addControl(new BMap.NavigationControl());   
 map.addControl(new BMap.ScaleControl());    
 map.addControl(new BMap.OverviewMapControl());    
-map.addControl(new BMap.MapTypeControl()); 
-map.enableScrollWheelZoom();   
+map.addControl(new BMap.MapTypeControl());  
+map.enableScrollWheelZoom();  
 map.setCurrentCity("杭州"); 
 
 var marker = new BMap.Marker(point);// 创建标注    
 map.addOverlay(marker);
+
+var transit = new BMap.TransitRoute(map, {
+	renderOptions: {
+		 map: map,
+		 panel: "list"
+		  }
+		});
+transit.search("杭州师范大学仓前新校区", "宾馆");
